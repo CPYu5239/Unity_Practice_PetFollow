0069e4432cc599ebca9b015041ff7af04616b551
CPYu5239 <57282801+CPYu5239@users.noreply.github.com>
2019-12-31 15:48:21 +0800

    建立C# Script

---
+++ UnityPetFollow/Assets/Scripts/PetFollow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class PetFollow : MonoBehaviour
+{
+    [Header("跟隨速度")]
+    public float speed = 7.5f;
+
+    private void LateUpdate()
+    {
+        PetTrack();
+    }
+
+    private void PetTrack()
+    {
+        Vector3 posPlayer = GameObject.Find("野蠻人").GetComponent<Transform>().position;
+
+        if (Vector3.Distance(posPlayer,transform.position) > 5)
+        {
+            transform.position = posPlayer;
+        }
+    }
+}
