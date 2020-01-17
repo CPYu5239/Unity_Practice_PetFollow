1f483db7e21a186b8aee20e5af9274f38fafd1d6
CPYu5239 <57282801+CPYu5239@users.noreply.github.com>
2020-01-17 18:00:11 +0800

    完成跟隨，並讓飛龍能夠看向跟隨點

--- UnityPetFollow/Assets/Scripts/PetFollow.cs
+++ UnityPetFollow/Assets/Scripts/PetFollow.cs
@@ -3,21 +3,30 @@ using UnityEngine;
 
 public class PetFollow : MonoBehaviour
 {
-    [Header("跟隨速度")]
+    [Header("跟隨速度"),Range(1f,15f)]
     public float speed = 7.5f;
+
+    private Transform followPoint;
+
+    private void Start()
+    {
+        followPoint = GameObject.Find("跟隨點").transform;
+    }
 
     private void LateUpdate()
     {
-        PetTrack();
+        if (Vector3.Distance(transform.position, followPoint.position) >= 3)
+        {
+            PetTrack();
+        }
     }
 
     private void PetTrack()
     {
-        Vector3 posPlayer = GameObject.Find("野蠻人").GetComponent<Transform>().position;
+        Vector3 posFollow = followPoint.position;
+        Vector3 posDragon = transform.position;
 
-        if (Vector3.Distance(posPlayer,transform.position) > 5)
-        {
-            transform.position = posPlayer;
-        }
+        transform.position = Vector3.Lerp(posDragon, posFollow, 0.5f * Time.deltaTime * speed);
+        transform.LookAt(posFollow);
     }
 }
